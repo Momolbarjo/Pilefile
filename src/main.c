2e0f47041d923366b67f1e2dbdbdff5c19e6a2d6
Momolbarjo <119139743+Momolbarjo@users.noreply.github.com>
2023-09-29 22:33:30 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -4,25 +4,67 @@
 
 int main(void)
 {
-	PileDyn myPile= NULL;
-	int val=0;
+	DynStack myStack= initializeStack();
+	DynStack myEvenStack = initializeStack();
+	DynStack myOddStack = initializeStack();
+	int valdestack;
+	int val=1;
+	
 	
 	for(int i=0 ; i<NB ; i++)
 	{
-		myPile=stack(val,myPile);
+		myStack=stack(val,myStack);
 		val++;
 	}
 	
+	printStack(myStack);
+
+	while(myStack != NULL){
+	myStack=destack(myStack,&valdestack);
+
+
+        //printf("Value destacked: %d\n", valdestack);
+    	if(valdestack%2==0){
+		myEvenStack=stack(valdestack,myEvenStack);
+	}
+	else{
+		myOddStack=stack(valdestack,myOddStack);
+		}
+
+	}
+
+	if (myStack == NULL) {
+
+        	printf("The stack is empty...\n");
+	  }
+    
+	printf("Even stack:\n");
+	printStack(myEvenStack);
+	printf("\n");
+	printf("Odd stack:\n");
+	printStack(myOddStack);
 	
-	while (myPile != NULL) 
+	while (myEvenStack != NULL) 
 	{
-        PileDyn temp = myPile;
-        myPile = myPile->next;
+        DynStack temp = myEvenStack;
+        myEvenStack = myEvenStack->next;
         free(temp);
-        }
+    	}
+	while (myOddStack != NULL) 
+	{
+        DynStack temp = myOddStack;
+        myOddStack = myOddStack->next;
+        free(temp);
+    	}
+	while (myStack != NULL) 
+	{
+        DynStack temp = myStack;
+        myStack = myStack->next;
+        free(temp);
+    	}
 	
 	return 0;
-}
+	}
 
 /* Ex1:
 
@@ -34,7 +76,6 @@ int main(void)
    a) [10,1,-3,12,8,7]
    b) [10,1,-3,7,8,12]
    
-  
 3) Il faut une pile pour enfiler les elements  et une autre pour defiler les elements(retirer de la file principale).
 
 */
