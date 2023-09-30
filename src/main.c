759a43e83f7328923d9a973b9a9d15307dce743c
Momolbarjo <119139743+Momolbarjo@users.noreply.github.com>
2023-09-30 22:39:53 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -3,15 +3,19 @@
 
 
 int main(void)
-{
+{ 	
+	StaticStack myEvenStackk,myOddStackk,myStackk;
 	DynStack myStack= initializeStack();
 	DynStack myEvenStack = initializeStack();
 	DynStack myOddStack = initializeStack();
 	int valdestack;
 	int val=1;
 	
+	initializeStaticStack(&myEvenStackk);
+	initializeStaticStack(&myOddStackk);
+	initializeStaticStack(&myStackk);
 	
-	for(int i=0 ; i<NB ; i++)
+	/*for(int i=0 ; i<NB ; i++)
 	{
 		myStack=stack(val,myStack);
 		val++;
@@ -24,7 +28,7 @@ int main(void)
 
 
         //printf("Value destacked: %d\n", valdestack);
-    	if(valdestack%2==0){
+    if(valdestack%2==0){
 		myEvenStack=stack(valdestack,myEvenStack);
 	}
 	else{
@@ -33,9 +37,9 @@ int main(void)
 
 	}
 
-	if (myStack == NULL) {
+if (myStack == NULL) {
 
-        	printf("The stack is empty...\n");
+        printf("The stack is empty...\n");
 	  }
     
 	printf("Even stack:\n");
@@ -44,27 +48,52 @@ int main(void)
 	printf("Odd stack:\n");
 	printStack(myOddStack);
 	
+
+	
 	while (myEvenStack != NULL) 
 	{
         DynStack temp = myEvenStack;
         myEvenStack = myEvenStack->next;
         free(temp);
-    	}
+    }
 	while (myOddStack != NULL) 
 	{
         DynStack temp = myOddStack;
         myOddStack = myOddStack->next;
         free(temp);
-    	}
+    }
 	while (myStack != NULL) 
 	{
         DynStack temp = myStack;
         myStack = myStack->next;
         free(temp);
-    	}
+    }*/
 	
-	return 0;
+	for(int i=0 ; i<NB ; i++)
+	{
+		push(&myStackk,val);
+		val++;
 	}
+
+	while(!isStackEmpty(&myStackk)){
+		int elmt=pop(&myStackk);
+		if(elmt %2 ==0){
+			push(&myEvenStackk,elmt);
+		}
+		else{
+			push(&myOddStackk,elmt);
+		}
+	}
+
+	printf("EvenStack : \n");
+	printStaticStack(&myEvenStackk);
+
+	printf("\n");
+
+	printf("OddStack : \n");
+	printStaticStack(&myOddStackk);
+	return 0;
+}
 
 /* Ex1:
 
@@ -76,6 +105,11 @@ int main(void)
    a) [10,1,-3,12,8,7]
    b) [10,1,-3,7,8,12]
    
+  
 3) Il faut une pile pour enfiler les elements  et une autre pour defiler les elements(retirer de la file principale).
 
+
+Ex 3:
+
+1) Une file dynamique
 */
