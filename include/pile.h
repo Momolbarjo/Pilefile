7b34bc780e7bc2d7f073acbfaa10aa8e03b76220
Momolbarjo <119139743+Momolbarjo@users.noreply.github.com>
2023-09-30 22:39:32 +0200

    Update pile.h

--- include/pile.h
+++ include/pile.h
@@ -3,6 +3,7 @@
 #define NB 20
 #include <stdlib.h>
 #include <time.h>
+#include <stdbool.h>
 #include <string.h>
 
 typedef struct  DynStackElement
@@ -11,10 +12,21 @@ typedef struct  DynStackElement
     struct DynStackElement* next;
 }DynStackElement, *DynStack;
 
+typedef struct {
+
+    int items[NB];
+    int head;
+} StaticStack;
+
 DynStack initializeStack(void);
 DynStack stack(int nb,DynStack sstack);
 void printStack(DynStack sstack);
+void initializeStaticStack(StaticStack* stack);
 DynStack destack(DynStack sstack, int* pvalue);
-
+bool isStackEmpty(StaticStack* stack);
+int pop(StaticStack* stack);
+void initializeStaticStack(StaticStack* stack);
+StaticStack* push( StaticStack *stack, int item);
+void printStaticStack(StaticStack *stack);
 
 #endif
