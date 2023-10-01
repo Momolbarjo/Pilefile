e79ab6dd880f19a0f968505e13e3aca86adea67c
Momolbarjo <119139743+Momolbarjo@users.noreply.github.com>
2023-10-01 22:38:20 +0200

    Update pile.h

--- include/pile.h
+++ include/pile.h
@@ -18,6 +18,16 @@ typedef struct {
     int head;
 } StaticStack;
 
+typedef struct QueueElement {
+    int data;
+    struct QueueElement* next;
+} QueueElement, *DynQueue;
+
+static QueueElement* first=NULL;
+static QueueElement* last=NULL;
+static int NbQueueElems= 0;
+
+
 DynStack initializeStack(void);
 DynStack stack(int nb,DynStack sstack);
 void printStack(DynStack sstack);
@@ -28,5 +38,14 @@ int pop(StaticStack* stack);
 void initializeStaticStack(StaticStack* stack);
 StaticStack* push( StaticStack *stack, int item);
 void printStaticStack(StaticStack *stack);
+bool isQueueEmpty(void);
+//DynQueue Client(DynQueue Client);
+int Queue_length(void);
+int Queue_first(void);
+int Queue_last(void);
+void print_Queue(void);
+void Enqueue(int x);
+void Dequeue(void);
+void clear_Queue(void);
 
 #endif
