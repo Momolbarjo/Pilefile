493ce8dd798df1e839876d4297f183c2eccee436
Momolbarjo <119139743+Momolbarjo@users.noreply.github.com>
2023-10-01 22:36:45 +0200

    Update pile.c

--- src/pile.c
+++ src/pile.c
@@ -72,7 +72,7 @@ int pop(StaticStack* stack){
         return stack->items[stack->head--];
     }
     else{
-        printf("The stack is empty");
+        printf("The stack is empty\n");
         return -1;
     }
 }
@@ -83,7 +83,7 @@ StaticStack* push( StaticStack *stack, int item){
         stack->items[++stack->head] = item;
     }
     else{
-        printf("There is no more place in the stack");
+        printf("There is no more place in the stack\n");
     }
     return stack;
 }
@@ -94,4 +94,100 @@ void printStaticStack(StaticStack *stack){
         printf("\n");
     }
     printf("\n");
+}
+
+
+bool isQueueEmpty(void){
+    if(last == NULL && first == NULL){
+        return true;
+    }
+    return false;
+}
+
+int Queue_length(void){
+    return NbQueueElems;
+}
+
+int Queue_first(void){
+    if(isQueueEmpty()){
+        exit(1);
+    }
+    return first->data;
+}
+
+int Queue_last(void){
+    if(isQueueEmpty()){
+        exit(1);
+    }
+    return last->data;
+}
+
+void print_Queue(void){
+    if(isQueueEmpty()){
+        printf("The Queue is empty\n");
+        return;
+    }
+    QueueElement* tmp=first;
+    
+    while(tmp != NULL){
+        printf("%d", tmp->data);
+        tmp=tmp->next;
+    }
+    printf("\n");
+}
+
+void Enqueue(int x){
+   QueueElement* element;
+
+    element= malloc(sizeof(*element));
+
+    if(element == NULL)
+    {
+        fprintf(stderr,"Error of memory allocation\n");
+        exit(1);
+
+    }
+
+    element->data = x;
+    element->next = NULL;
+
+    if(isQueueEmpty()){
+        first = element;
+        last = element;
+    }
+    else{
+        last->next = element;
+        last = element;
+    }
+        NbQueueElems++;
+}
+
+void Dequeue(void){
+    if(isQueueEmpty()){
+        printf("The Queue is empty\n");
+        return;
+    }
+
+    QueueElement* tmp = first;
+
+    if(first == last){
+        first = NULL;
+        last = NULL;
+    }
+    else{
+        first = first->next;
+    }
+    free(tmp);
+    NbQueueElems--;
+}
+
+void clear_Queue(void){
+    if(isQueueEmpty()){
+        printf("The Queue is empty\n");
+        return;
+    }
+    while(!isQueueEmpty()){
+        Dequeue();
+    }
+
 }
